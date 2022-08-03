af7dffaa4e2004778fa63f9d1f4fb5abd35e4174
Leon <82407168+sed-i@users.noreply.github.com>
2022-08-03 14:16:07 +0000

    Add k8s resource limit patch lib (#19)

--- README.md
+++ README.md
@@ -7,22 +7,32 @@ are used for the development of charms deployed as part of the LMA Light [bundle
 
 ## Usage
 
-While it is possible to deploy this charm, it is essentially a no-op, and not what this charm was
-designed for. Instructions are omitted. This may change in the near future.
+While it is possible to deploy this charm, it is only a tester charm and is
+intended to be deployed only for testing purposes.
+
+```shell
+charmcraft pack
+juju deploy ./observability-libs_ubuntu-20.04-amd64.charm \
+  --resource placeholder-image=busybox
+```
 
 Where this charm should be used, is to access one of the following libraries during development:
 
 - [KubernetesServicePatch] - a small library used to patch the Juju auto-created Kubernetes Service
   during the deployment of a sidecar charm to contain the correct ports for an application.
 
-- [JujuTopology] - Used to create and output Juju topologies either from charms, relation data, or parts. 
+- [KubernetesComputeResourcesPatch] - a small library used to patch the Juju
+  auto-created statefulset with custom resource limits.
+
+- [JujuTopology] - Used to create and output Juju topologies either from charms, relation data, or parts.
 
 ## Contributing
 
 Please see the [Juju SDK docs](https://juju.is/docs/sdk) for guidelines on enhancements to this
 charm following best practice guidelines, and `CONTRIBUTING.md` for developer guidance.
 
-[kubernetesservicepatch]: https://charmhub.io/observability-libs/libraries/kubernetes_service_patch
+[KubernetesServicePatch]: https://charmhub.io/observability-libs/libraries/kubernetes_service_patch
+[KubernetesComputeResourcesPatch]: https://charmhub.io/observability-libs/libraries/kubernetes_compute_resources_patch
 [bundle]: https://charmhub.io/lma-light
 [charm libraries]: https://juju.is/docs/sdk/libraries
 [JujuTopology]: https://charmhub.io/observability-libs/libraries/juju_topology
---
+++ tests/unit/test_kubernetes_compute_resources.py
@@ -0,0 +1,105 @@
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+import unittest
+from unittest import mock
+from unittest.mock import Mock
+
+from charms.observability_libs.v0.kubernetes_compute_resources_patch import (
+    KubernetesComputeResourcesPatch,
+    adjust_resource_requirements,
+    is_valid_spec,
+    sanitize_resource_spec_dict,
+)
+from ops.charm import CharmBase
+from ops.testing import Harness
+
+CL_PATH = "charms.observability_libs.v0.kubernetes_compute_resources_patch.KubernetesComputeResourcesPatch"
+
+
+class TestKubernetesComputeResourcesPatch(unittest.TestCase):
+    class _TestCharm(CharmBase):
+        def __init__(self, *args):
+            super().__init__(*args)
+            self.resources_patch = KubernetesComputeResourcesPatch(
+                self,
+                "placeholder",
+                resource_reqs_func=lambda: adjust_resource_requirements(None, None),
+            )
+            self.framework.observe(self.resources_patch.on.patch_failed, self._patch_failed)
+            self.patch_failed_counter = 0
+
+        def _patch_failed(self, _):
+            self.patch_failed_counter += 1
+
+    def setUp(self) -> None:
+        self.harness = Harness(
+            self._TestCharm, meta=open("metadata.yaml"), config=open("config.yaml")
+        )
+
+    @mock.patch("lightkube.core.client.GenericSyncClient", Mock)
+    @mock.patch(f"{CL_PATH}._namespace", "test-namespace")
+    def test_listener_is_attached_for_config_changed_event(self):
+        self.harness.begin()
+        charm = self.harness.charm
+        with mock.patch(f"{CL_PATH}._patch") as patch:
+            charm.on.config_changed.emit()
+            self.assertEqual(patch.call_count, 1)
+
+    @mock.patch("lightkube.core.client.GenericSyncClient", Mock)
+    @mock.patch(f"{CL_PATH}._namespace", "test-namespace")
+    def test_patch_is_applied_regardless_of_leadership_status(self):
+        self.harness.begin()
+        charm = self.harness.charm
+        for is_leader in (True, False):
+            with self.subTest(is_leader=is_leader):
+                self.harness.set_leader(True)
+                with mock.patch(f"{CL_PATH}._patch") as patch:
+                    charm.on.config_changed.emit()
+                    self.assertEqual(patch.call_count, 1)
+
+    @mock.patch.object(KubernetesComputeResourcesPatch, "_namespace", "test-namespace")
+    @mock.patch("lightkube.core.client.GenericSyncClient")
+    def test_patch_is_applied_during_startup_sequence(self, client_mock):
+        self.harness.begin_with_initial_hooks()
+        self.assertGreater(client_mock.call_count, 0)
+
+    @mock.patch.object(KubernetesComputeResourcesPatch, "_namespace", "test-namespace")
+    @mock.patch("lightkube.core.client.GenericSyncClient")
+    def test_invalid_config_emits_custom_event(self, client_mock):
+        self.harness.begin_with_initial_hooks()
+
+        # Test invalid quantity values
+        for (cpu, memory) in [
+            ("-1", ""),
+            ("", "-1Gi"),
+            ("-1", "1Gi"),
+            ("1", "-1Gi"),
+            ("4x", "1Gi"),
+            ("1", "1Gx"),
+        ]:
+            with self.subTest(cpu=cpu, memory=memory):
+                before = self.harness.charm.patch_failed_counter
+                self.harness.update_config({"cpu": cpu, "memory": memory})
+                after = self.harness.charm.patch_failed_counter
+                self.assertGreater(after, before)
+
+
+class TestResourceSpecDictValidation(unittest.TestCase):
+    def test_sanitize_resource_spec_dict(self):
+        self.assertEqual(None, sanitize_resource_spec_dict(None))
+        self.assertEqual({}, sanitize_resource_spec_dict({}))
+        self.assertEqual({"bad": "combo"}, sanitize_resource_spec_dict({"bad": "combo"}))
+        self.assertEqual({"cpu": 1}, sanitize_resource_spec_dict({"cpu": 1}))
+        self.assertEqual({"cpu": "1"}, sanitize_resource_spec_dict({"cpu": "1"}))
+        self.assertEqual({"memory": "858993460"}, sanitize_resource_spec_dict({"memory": "0.8Gi"}))
+
+    def test_is_valid_spec(self):
+        self.assertTrue(is_valid_spec(None))
+        self.assertTrue(is_valid_spec({}))
+        self.assertTrue(is_valid_spec({"cpu": "1"}))
+        self.assertTrue(is_valid_spec({"memory": "858993460"}))
+        self.assertTrue(is_valid_spec({"memory": "0.8Gi"}))
+        self.assertTrue(is_valid_spec({"cpu": None, "memory": None}))
+
+        self.assertFalse(is_valid_spec({"bad": "combo"}))
+        self.assertFalse(is_valid_spec({"invalid-key": "1"}))
