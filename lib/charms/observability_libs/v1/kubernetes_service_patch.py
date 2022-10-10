ad65bb214bbd9e578adcec154f068fd0eb9cb2f8
Maksim Beliaev <beliaev.m.s@gmail.com>
2022-10-10 16:33:46 +0200

    bump version

--- lib/charms/observability_libs/v1/kubernetes_service_patch.py
+++ lib/charms/observability_libs/v1/kubernetes_service_patch.py
@@ -146,7 +146,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 3
+LIBPATCH = 4
 
 ServiceType = Literal["ClusterIP", "LoadBalancer"]
 
