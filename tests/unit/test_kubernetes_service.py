5c64cc3a8e1cb3815eb13fb7fd209592bbd0c5d3
Maksim Beliaev <beliaev.m.s@gmail.com>
2022-10-10 16:55:00 +0200

    add test for event

--- tests/unit/test_kubernetes_service.py
+++ tests/unit/test_kubernetes_service.py
@@ -40,7 +40,7 @@ class _TestCharm(CharmBase):
         super().__init__(*args)
         svc1 = ServicePort(1234, name="svc1", targetPort=1234)
         svc2 = ServicePort(1235, name="svc2", targetPort=1235)
-        self.service_patch = KubernetesServicePatch(self, [svc1, svc2])
+        self.service_patch = KubernetesServicePatch(self, [svc1, svc2], refresh_event=self.on.config_changed)
 
 
 class _TestCharmCustomServiceName(CharmBase):
@@ -358,6 +358,13 @@ class TestK8sServicePatch(unittest.TestCase):
         )
 
         self.assertEqual(service_patch.service, expected_service)
+
+    def test_custom_event_is_fired(self):
+        """Check that events provided via refresh_event are called"""
+        charm = self.harness.charm
+        with mock.patch(f"{CL_PATH}._patch") as patch:
+            charm.on.config_changed.emit()
+            self.assertEqual(patch.call_count, 1)
 
     def test_given_initialized_charm_when_install_event_then_event_listener_is_attached(self):
         charm = self.harness.charm
