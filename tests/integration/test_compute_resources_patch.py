b6dca3142f346f66dae40e4688afa47c02602673
sed-i <82407168+sed-i@users.noreply.github.com>
2023-02-11 01:39:39 +0200

    Do not pass series

--- tests/integration/test_compute_resources_patch.py
+++ tests/integration/test_compute_resources_patch.py
@@ -45,7 +45,6 @@ async def test_build_and_deploy(ops_test: OpsTest, o11y_libs_charm):
         o11y_libs_charm,
         resources=resources,
         application_name=app_name,
-        series="focal",
         trust=True,
     )
 
