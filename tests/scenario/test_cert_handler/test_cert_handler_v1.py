2c0bd771b7c3304ee4d881323bde263016f1342d
Pietro Pasotti <starfire.daemon@gmail.com>
2024-06-14 14:37:25 +0200

    parametrize peer relation name for vault backend storage and add a test for it

--- tests/scenario/test_cert_handler/test_cert_handler_v1.py
+++ tests/scenario/test_cert_handler/test_cert_handler_v1.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import pytest
 from ops import CharmBase
-from scenario import Context, Relation, State
+from scenario import Context, PeerRelation, Relation, State
 
 from lib.charms.observability_libs.v1.cert_handler import (
     CertHandler,
@@ -40,6 +40,35 @@ def certificates():
 def test_cert_joins(ctx, certificates, leader):
     with ctx.manager(
         certificates.joined_event, State(leader=leader, relations=[certificates], secrets=[])
+    ) as mgr:
+        mgr.run()
+        assert mgr.charm.ch.private_key
+
+
+class MyJuju2Charm(CharmBase):
+    META = {
+        "name": "fabio",
+        "requires": {"certificates": {"interface": "certificates"}},
+        "peers": {"myfunkypeers": {"interface": "peerymcpeer"}},
+    }
+
+    def __init__(self, fw):
+        super().__init__(fw)
+        self.ch = CertHandler(
+            self, key="ch", sans=[socket.getfqdn()], peer_relation_name="myfunkypeers"
+        )
+
+
+@pytest.fixture
+def ctx_juju2():
+    return Context(MyJuju2Charm, MyJuju2Charm.META, juju_version="2.0")
+
+
+@pytest.mark.parametrize("leader", (True, False))
+def test_cert_joins_peer_vault_backend(ctx_juju2, certificates, leader):
+    with ctx_juju2.manager(
+        certificates.joined_event,
+        State(leader=leader, relations=[certificates, PeerRelation("myfunkypeers")], secrets=[]),
     ) as mgr:
         mgr.run()
         assert mgr.charm.ch.private_key
