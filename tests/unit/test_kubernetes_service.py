ae48c34dfd13440f1ac0b556e2054a6712d5b184
IbraAoad <Ibrahim.Awwad@canonical.com>
2024-07-09 23:54:46 +0300

    now the linter

--- tests/unit/test_kubernetes_service.py
+++ tests/unit/test_kubernetes_service.py
@@ -376,7 +376,9 @@ class TestK8sServicePatch(unittest.TestCase):
 
     @patch(f"{CL_PATH}._namespace", "test")
     @patch(f"{MOD_PATH}.Client")
-    def test_given_initialized_charm_when_upgrade_event_then_event_listener_is_attached(self,client):
+    def test_given_initialized_charm_when_upgrade_event_then_event_listener_is_attached(
+        self, client
+    ):
         charm = self.harness.charm
         client.return_value = client
         client.list.return_value = []
